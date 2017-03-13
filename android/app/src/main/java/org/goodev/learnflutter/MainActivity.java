9cea9a92b30cf66157446d3df0e29788d66c0764
goodev <adming@goodev.org>
2017-03-13 19:28:29 +0800

    3. Change App name and add doc for FlutterActivity

--- android/app/src/main/java/org/goodev/learnflutter/MainActivity.java
+++ android/app/src/main/java/org/goodev/learnflutter/MainActivity.java
@@ -3,6 +3,8 @@ package org.goodev.learnflutter;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 
+// FlutterActivity 代码位于 https://github.com/flutter/engine/blob/master/shell/platform/android/io/flutter/app/FlutterActivity.java
+// 里面使用了全屏的 FlutterView， 在 FlutterView  运行 Flutter app 代码。
 public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
